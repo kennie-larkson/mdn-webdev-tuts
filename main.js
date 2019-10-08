ec02475d4289196a0a604785cdf9a0dd74bd3895
LAWAL ABD'KEHINDE <32939546+kennie-larkson@users.noreply.github.com>
2019-10-08 11:23:15 +0100

    first commit on this repository

---
+++ main.js
@@ -0,0 +1,14 @@
+
+const firstDiv = document.querySelector('#first-div');
+const secondDiv = document.querySelector('#second-div');
+
+const button = document.querySelector('button');
+button.style.backgroundColor = 'yellow';
+
+function movement(){
+
+    
+        firstDiv.style.left = '50px';
+        secondDiv.style.left = '50px';
+}
+
